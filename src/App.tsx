60539c34fbc7bd113d8f5e44ce968c6456cf8cee
Yuval Starnes <yuval@sensusmd.com>
2021-09-09 20:29:40 +0300

    hooks for socket

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,28 @@ import Tree, { NodeList } from '@naisutech/react-tree'
 import io from 'socket.io-client'
 
 
-const socket = io('http://localhost:3000', { transports: ['websocket', 'polling', 'flashsocket'] });
+const socket = io('http://localhost:3005', { transports: ['websocket', 'polling', 'flashsocket'] });
 
 
 
 
 
     function App() {
-        let data: { id: number, parentId: any, label:string }[] = [];
+        const [data, setData] = useState([]);
+
         useEffect(()=> {
             socket.on('tree', (tree)=>{
-                    data.length = 0;
-                    data.push(...tree)
+                    setData(tree)
+
             }
             );
         });
   return (
     <div className="App">
       <header className="App-header">
-
         <Tree nodes={data} />
       </header>
+
     </div>
   );
 }
