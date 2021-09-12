9161fbcd3d0b6095f9d93d15d277e5f07bf71503
Yuval Starnes <yuval@sensusmd.com>
2021-09-12 03:22:40 +0300

    unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 
 import './App.css';
-import Tree, {NodeList} from '@naisutech/react-tree'
+import Tree from '@naisutech/react-tree'
 import io from 'socket.io-client'
 
 
@@ -15,7 +15,7 @@ function App() {
     useEffect(() => {
         socket.on('tree', (tree) => {
             setMasterData(tree)
-            setData(masterData)
+            setData(tree)
         });
     });
 
@@ -23,10 +23,12 @@ function App() {
     return (
         <div className="App">
             <div className="App-header">
-      <div className="">
-          <button onClick={() => setData(masterData.filter(item => item["bDeviceClass"] === 9))}>Show Only Hubs</button>
-          <button onClick={() => setData(masterData)}>Show All</button>
-      </div>
+                <div className="">
+                    <button onClick={() => setData(masterData.filter(item => item["bDeviceClass"] === 9))}>Show Only
+                        Hubs
+                    </button>
+                    <button onClick={() => setData(masterData)}>Show All</button>
+                </div>
                 <Tree nodes={data}/>
             </div>
 
